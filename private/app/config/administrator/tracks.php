7c3a4ab6a9a28a071d7c01b31b771b84f4d4b79e
Manatsawin Hanmongkolchai <manatsawin+gitywc@gmail.com>
2015-01-10 10:45:46 +0700

    Added admin app

---
+++ private/app/config/administrator/tracks.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	'title' => 'Tracks',
+	'single' => 'track',
+	'model' => 'Track',
+	'columns' => array(
+		'name', 'description', 'file', 'instrument', 'permission'
+	),
+	'edit_fields' => array(
+		'name', 'description', 'file', 'instrument', 'permission'
+	)
+);
